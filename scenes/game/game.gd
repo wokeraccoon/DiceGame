fc4e7c1daea96d62541f0920bc9592ac88b4de99
wokeraccoon <13603525+wokeraccoon@users.noreply.github.com>
2024-12-30 16:56:41 -0500

    finished combos and score calculation

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -6,7 +6,7 @@ extends Node2D
 
 const ITEM_PICKER = preload("res://scenes/game/ui/item_picker/item_picker.tscn")
 
-enum DiceCombos {
+enum DiceCombosNames {
 	SINGLE,
 	PAIR,
 	TWO_PAIR,
@@ -30,6 +30,9 @@ var current_dice_hand : Dictionary = {
 	"dice_five" : 1,
 }
 
+var current_dice_combos_on_hand : Dictionary = {}
+var total_dice_hand_score : float = 0
+
 var current_enemy : Enemy
 
 var enemy_health : int = 0
@@ -49,26 +52,14 @@ var games_state : GameStates = GameStates.RUN_START
 var player_weapon : Weapon
 var player_inventory : Array[Item]
 
-func _ready() -> void:
-	print("")
-	var current_dice : int = 1
-	for dice in current_dice_hand:
-		current_dice_hand[dice] = randi_range(1,6)
-		print(str("Dice ",current_dice,": ",current_dice_hand[dice]))
-		current_dice += 1
-
-	print(" ")
-	check_for_dice_combos(current_dice_hand)
 
 func count_value_occurrences_concise(my_dictionary: Dictionary, target_value) -> int:
 	return my_dictionary.values().count(target_value)
 
-
-func check_for_dice_combos(dice_rolled : Dictionary) -> void:
-
-	var dice_sides_ammounts : Dictionary = {
-
-	}
+func check_for_dice_combos(dice_rolled : Dictionary) -> int:
+	
+	var current_dice_hand_score : int = 0
+	var dice_sides_ammounts : Dictionary = {}
 
 	for dice_value in 6:
 		dice_value += 1
@@ -76,34 +67,36 @@ func check_for_dice_combos(dice_rolled : Dictionary) -> void:
 		if ocurrences > 0:
 			dice_sides_ammounts[dice_value] = ocurrences
 
-	print(dice_sides_ammounts)
-
 	#yatch
 	if dice_sides_ammounts.size() == 1:
-		print("Yatch!")
-
-	elif dice_sides_ammounts.has(2) and dice_sides_ammounts.has(3) and dice_sides_ammounts.has(4) and dice_sides_ammounts.has(5) and dice_sides_ammounts.has(6):
-		print("Large Straight!")
-
-	elif dice_sides_ammounts.has(1) and dice_sides_ammounts.has(2) and dice_sides_ammounts.has(3) and dice_sides_ammounts.has(4) and dice_sides_ammounts.has(5):
-			print("Little Straight!")
+		current_dice_combos_on_hand[DiceCombosNames.keys()[DiceCombosNames.YATCH]] = 50
+	
+	#straights
+	if dice_sides_ammounts.has(2) and dice_sides_ammounts.has(3) and dice_sides_ammounts.has(4) and dice_sides_ammounts.has(5) and dice_sides_ammounts.has(6):
+		current_dice_combos_on_hand[DiceCombosNames.keys()[DiceCombosNames.STRAIGHT]] = 35
+		
+	if dice_sides_ammounts.has(1) and dice_sides_ammounts.has(2) and dice_sides_ammounts.has(3) and dice_sides_ammounts.has(4) and dice_sides_ammounts.has(5):
+			current_dice_combos_on_hand[DiceCombosNames.keys()[DiceCombosNames.STRAIGHT]] = 35
 
 	#full house or four on a row
-	elif dice_sides_ammounts.size() == 2:
+	if dice_sides_ammounts.size() == 2:
 
 		var set_one : int = dice_sides_ammounts[dice_sides_ammounts.keys()[0]]
 		var set_two : int = dice_sides_ammounts[dice_sides_ammounts.keys()[1]]
-
+		
 		#full house
 		if set_one == 2 or set_one == 3:
 			if set_two == 2 or set_two == 3:
-				print("Full House!")
+				current_dice_combos_on_hand[DiceCombosNames.keys()[DiceCombosNames.FULL_HOUSE]] = 25
 
 		#four on a row
-		if set_one == 4 or set_two == 4:
-			print("Four on a row!")
+		if set_one == 4:
+			current_dice_combos_on_hand[DiceCombosNames.keys()[DiceCombosNames.FOUR_OF_A_KIND]] = 30
+		elif set_two == 4:
+			current_dice_combos_on_hand[DiceCombosNames.keys()[DiceCombosNames.FOUR_OF_A_KIND]] = 30
 
 	else:
+		#pair, two pair, and tree of a kind
 		var pair_ammount : int = 0
 		var has_trio : bool = false
 
@@ -114,13 +107,23 @@ func check_for_dice_combos(dice_rolled : Dictionary) -> void:
 				has_trio = true
 
 		if pair_ammount == 1:
-			print("Pair!")
+			current_dice_combos_on_hand[DiceCombosNames.keys()[DiceCombosNames.PAIR]] = 5
 		elif pair_ammount == 2:
-			print("Two Pair!")
+			current_dice_combos_on_hand[DiceCombosNames.keys()[DiceCombosNames.TWO_PAIR]] = 10
 		elif has_trio:
-			print("Tree of a kind!")
-		else:
-			print("Nothing but singles!")
+			current_dice_combos_on_hand[DiceCombosNames.keys()[DiceCombosNames.THREE_OF_A_KIND]] = 15
+	
+	#single dice
+	var single_dice_value : int = 0
+	for dice in current_dice_hand:
+		single_dice_value += current_dice_hand[dice]
+	
+	current_dice_hand_score = single_dice_value
+	
+	if current_dice_combos_on_hand.size() != 0:
+		current_dice_hand_score *= current_dice_combos_on_hand[current_dice_combos_on_hand.keys()[0]]
+	
+	return current_dice_hand_score
 
 func _process(_delta: float) -> void:
 
