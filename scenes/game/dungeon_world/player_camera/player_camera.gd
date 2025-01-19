811153ff48db7411b5dd84676a2acaf7636a521a
wokeraccoon <13603525+wokeraccoon@users.noreply.github.com>
2025-01-19 16:25:07 -0500

    improved player control code in dungeon

--- scenes/game/dungeon_world/player_camera/player_camera.gd
+++ scenes/game/dungeon_world/player_camera/player_camera.gd
@@ -1,29 +1,62 @@
 class_name PlayerCamera
 extends CharacterBody3D
 
-var input_target_rotation : float = 0
-var input_target_position : Vector3 = Vector3.ZERO
+@onready var forward_solid_detector: RayCast3D = $ForwardSolidDetector
+@onready var back_solid_detector: RayCast3D = $BackSolidDetector
 
-func _physics_process(delta: float) -> void:
+
+enum MovementStates {
+	IDLE,
+	WALK,
+	ROTATE
+}
+
+var movement_state : MovementStates = MovementStates.IDLE
+
+var target_pos : Vector3 = Vector3.ZERO
+var target_angle : float = 0.0
+
+func _process(delta: float) -> void:
 	
-	if global_position == input_target_position and snappedf(rad_to_deg(global_rotation.y) - snappedf(rad_to_deg(global_rotation.y),0.1),0.01) == 0:
-		if Input.is_action_pressed("walk_forward"):
-			input_target_position = global_position - (transform.basis * Vector3(0,0,2))
+	match movement_state:
+		MovementStates.IDLE:
 			
-		if Input.is_action_pressed("walk_backwards"):
-			input_target_position = global_position + (transform.basis * Vector3(0,0,2))
-		
-		if Input.is_action_just_pressed("turn_left"):
-			input_target_rotation += deg_to_rad(90)
+			target_angle = 0
 			
-		if Input.is_action_just_pressed("turn_right"):
-			input_target_rotation -= deg_to_rad(90)
+			if Input.is_action_pressed("walk_forward") :
+				if !forward_solid_detector.is_colliding():
+					target_pos = global_position - (transform.basis * Vector3(0,0,2))
+					movement_state = MovementStates.WALK
+			elif Input.is_action_pressed("walk_backwards") :
+				if !back_solid_detector.is_colliding():
+					target_pos = global_position - (transform.basis * Vector3(0,0,-2))
+					movement_state = MovementStates.WALK
+				
+			if Input.is_action_pressed("turn_left"):
+				target_angle = global_rotation.y + deg_to_rad(90)
+				movement_state = MovementStates.ROTATE
+			elif Input.is_action_pressed("turn_right"):
+				target_angle = global_rotation.y - deg_to_rad(90)
+				movement_state = MovementStates.ROTATE
+			
+		MovementStates.WALK:
+			global_position = lerp(global_position,target_pos,delta * 20)
+			
+			if global_position.is_equal_approx(target_pos):
+				global_position = target_pos
+				movement_state = MovementStates.IDLE
+			
+		MovementStates.ROTATE:
+			
+			if is_equal_approx(270, rad_to_deg(target_angle)): target_angle = deg_to_rad(-90)
+			elif is_equal_approx(-270, rad_to_deg(target_angle)): target_angle = deg_to_rad(90)
+			
+			global_rotation.y = lerp_angle(global_rotation.y, target_angle, delta * 20)
+			
+			if is_equal_approx(global_rotation.y, target_angle):
+				global_rotation.y = target_angle
+				target_angle = 0
+				movement_state = MovementStates.IDLE
 				
 	
-	global_rotation.y = lerp_angle(global_rotation.y, input_target_rotation, delta * 20)
-
-	global_position = lerp(global_position,input_target_position,delta * 10)
-	
-	if input_target_position.distance_to(global_position) <= 0.1:
-		global_position = input_target_position
 	
