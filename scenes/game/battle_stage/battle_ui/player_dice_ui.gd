16465aa51d2f9c08f75683e2d6bf53ffd9169db6
wokeraccoon <13603525+wokeraccoon@users.noreply.github.com>
2025-02-03 00:18:14 -0500

    finished battle ui basics

--- scenes/game/battle_stage/battle_ui/player_dice_ui.gd
+++ scenes/game/battle_stage/battle_ui/player_dice_ui.gd
@@ -6,17 +6,18 @@ const DICE: PackedScene = preload("res://dice/dice.tscn")
 @onready var player_dice_holder: HBoxContainer = %PlayerDiceHolder
 @onready var roll_button: Button = %RollButton
 @onready var attack_button: Button = %AttackButton
-@onready var player_stats: HBoxContainer = %PlayerStats
 @onready var scoring_area: HBoxContainer = %ScoringArea
+@onready var top_bar_ui: MarginContainer = %TopBarUI
 
 @export var player_dice : Array[Dice] = []
 
 signal dice_roll_finished(dice_values : Dictionary[String,int])
 
 func _ready() -> void:
+	
+	
 	roll_button.hide()
 	attack_button.hide()
-	player_stats.hide()
 	scoring_area.hide()
 	
 	_on_battle_stage_ready()
@@ -32,10 +33,9 @@ func _on_battle_stage_ready() -> void:
 		
 	
 	await get_tree().create_timer(0.25).timeout
-	dice_hand_label.text = "[shake rate=20.0 level=5 connected=1][font_size=64]Rolling...[/font_size][/shake]"
+	dice_hand_label.text = "[shake rate=20.0 level=5 connected=1][font_size=48]Rolling...[/font_size][/shake]"
 	roll_button.show()
 	attack_button.show()
-	player_stats.show()
 	scoring_area.show()
 	roll_button.disabled = true
 	attack_button.disabled = true
@@ -61,32 +61,31 @@ func on_dice_roll_complete(_number : int) -> void:
 		dice_roll_finished.emit(dice_values)
 		
 		
-		
 @onready var dice_hand_label: RichTextLabel = %DiceHandLabel
 
 func show_dice_combo(dice_combo : DiceCalculator.DiceComboNames) -> void:
 	
 	match dice_combo:
 		DiceCalculator.DiceComboNames.SINGLES:
-			dice_hand_label.text = "[font_size=64]Singles[/font_size]"
+			dice_hand_label.text = "[font_size=48]Singles (1)[/font_size]"
 		DiceCalculator.DiceComboNames.PAIR:
-			dice_hand_label.text = "[font_size=64]Pair[/font_size]"
+			dice_hand_label.text = "[font_size=48]Pair (5)[/font_size]"
 		DiceCalculator.DiceComboNames.TWO_PAIR:
-			dice_hand_label.text = "[font_size=64]Two Pair[/font_size]"
+			dice_hand_label.text = "[font_size=48]Two Pair (10)[/font_size]"
 		DiceCalculator.DiceComboNames.THREE_OF_A_KIND:
-			dice_hand_label.text = "[wave amp=50 freq=5][font_size=64]Three of a kind[/font_size][/wave]"
+			dice_hand_label.text = "[font_size=48]Three of a kind (20)[/font_size]"
 		DiceCalculator.DiceComboNames.FOUR_OF_A_KIND:
-			dice_hand_label.text = "[wave amp=50 freq=5][font_size=64]Four of a kind[/font_size][/wave]"
+			dice_hand_label.text = "[font_size=48]Four of a kind (30)[/font_size]"
 		DiceCalculator.DiceComboNames.FULL_HOUSE:
-			dice_hand_label.text = "[wave amp=50 freq=5][font_size=64]Full house[/font_size][/wave]"
+			dice_hand_label.text = "[font_size=48]Full house (25)[/font_size]"
 		DiceCalculator.DiceComboNames.STRAIGHT:
-			dice_hand_label.text = "[wave amp=50 freq=5][font_size=64]Straight[/font_size][/wave]"
+			dice_hand_label.text = "[font_size=48]Straight (35)[/font_size]"
 		DiceCalculator.DiceComboNames.YATCH:
-			dice_hand_label.text = "[rainbow sat=0.5][wave amp=100 freq=5][font_size=64]Yatch![/font_size][/wave][/rainbow]"
+			dice_hand_label.text = "[rainbow sat=0.5][wave amp=100 freq=5][font_size=48]Yatch! (50)[/font_size][/wave][/rainbow]"
 
 func _on_roll_button_pressed() -> void:
 	
-	dice_hand_label.text = "[shake rate=20.0 level=5 connected=1][font_size=64]Rolling...[/font_size][/shake]"
+	dice_hand_label.text = "[shake rate=20.0 level=5 connected=1][font_size=48]Rolling...[/font_size][/shake]"
 	
 	for dice : Dice in player_dice:
 		dice.start_roll_dice()
--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -3,11 +3,15 @@ extends Control
 
 @onready var quit_button: Button = %QuitButton
 
+@onready var new_game_button: Button = %NewGameButton
+
 signal new_game_started
 
 func _ready() -> void:
 	if OS.has_feature("android"):
 		quit_button.hide()
+	
+	new_game_button.grab_focus()
 
 func _on_new_game_button_pressed() -> void:
 	new_game_started.emit()
